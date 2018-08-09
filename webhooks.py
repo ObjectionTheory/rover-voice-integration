083cbbe02d8e4354c7615d429b8b4fb891e8ab66
ObjectionTheory <therandomguy265@gmail.com>
2018-08-09 13:54:07 +0100

    Hrm

--- webhooks.py
+++ webhooks.py
@@ -4,8 +4,9 @@ import requests
 def processRequest(req):
     print("Request:")
     print(json.dumps(req, indent=4))
-    print(req.get("queryResult").get("intent"))
-    if req.get("queryResult").get("intent") == "projects/oftheinstrument/agent/intents/4bd6e615-a3f2-43a0-aec3-486e824ef015":
+
+    
+    if req.get("result").get("metadata").get("intentName") == "Sally Forth!":
         print("Yeet")
         res = speech("Sallying forth!")
     else:
