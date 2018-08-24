aa1156f88c193511e074cd10a21d6c76ae5f5314
ObjectionTheory <therandomguy265@gmail.com>
2018-08-24 15:17:32 +0100

    Yeet

--- wsgi.py
+++ wsgi.py
@@ -133,8 +133,8 @@ def assignCommand(req, left=0, right=0, claw=0):
 
     roverid = currentRover
 
-    if len(req["result"]["parameters"]["duration"]) > 1:
-        duration = req["result".get("parameters").get("duration")
+    if req["result"]["parameters"].get("duration"):
+        duration = req["result"]["parameters"]["duration"]
     
         if duration["unit"] == "s":
             travelTime = duration["amount"]
